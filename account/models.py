29faa05ac6cc2f0e252dbe22e1c1cd0fa8899232
whistler228 <dpava111@yahoo.co.jp>
2020-10-01 16:17:08 +0900

    add CustomUser

---
+++ account/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from core.models import Gift, GiftType
+
+
+class CustomUser(AbstractUser):
+    nickname = models.CharField(max_length=12)
+    notified = models.ManyToManyField(Gift, related_name="notified_users")
+
+
+class Condition(models.Model):
+    user = models.ForeignKey("CustomUser", on_delete=models.CASCADE, related_name="condition")
+    gift_type = models.ForeignKey(GiftType, on_delete=models.CASCADE, related_name="cond_gift_type")
+    min_price = models.IntegerField(null=True)
+    max_price = models.IntegerField(null=True)
+    max_rate = models.IntegerField(null=True)
