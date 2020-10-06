5ec4c9fd5e9293d7303587aa39e9086447f20f2d
whistler228 <dpava111@yahoo.co.jp>
2020-10-06 23:40:30 +0900

    add admin

--- account/admin.py
+++ account/admin.py
@@ -1,1 +1,6 @@
-# Register your models here.
+from django.contrib import admin
+
+from account.models import CustomUser, Condition
+
+admin.site.register(CustomUser)
+admin.site.register(Condition)
--- notify/admin.py
+++ notify/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import Gift, GiftType
+
+admin.site.register(Gift)
+admin.site.register(GiftType)
