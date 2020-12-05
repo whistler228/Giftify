673b8fb3e3aca410527b7b47fa24fb1c81dce7c9
whistler228 <dpava111@yahoo.co.jp>
2020-12-06 01:53:55 +0900

    moved CustomForm to forms.py

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,38 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+from django.forms import CharField
 
 from account.models import CustomUser
+
+
+class CustomUserCreateForm(UserCreationForm):
+    email = CharField(required=True, max_length=254, label="E-Mail address")
+    nickname = CharField(required=True, max_length=12, label="Nickname")
+
+    class Meta:
+        model = CustomUser
+        fields = ("username", "nickname", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(CustomUserCreateForm, self).save(commit=False)
+        user.nickname = self.cleaned_data["nickname"]
+        if commit:
+            user.save()
+        return user
+
+    def clean_nickname(self):
+        if not self.cleaned_data["nickname"]:
+            raise ValidationError("Please Enter your nickname.")
+        if len(self.cleaned_data["nickname"]) > 12:
+            raise ValidationError("Nickname must be <= 12 char.")
+        return self.cleaned_data["nickname"]
+
+
+class CustomUserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ("nickname", "email")
 
 
 class UnsubscribeForm(forms.Form):
--- account/views.py
+++ account/views.py
@@ -1,40 +1,14 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
-from django.core.exceptions import ValidationError
-from django.forms import CharField
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic
 
-from .forms import UnsubscribeForm
+from .forms import CustomUserCreateForm, UnsubscribeForm, CustomUserUpdateForm
 from .models import CustomUser
 
 
-class CustomForm(UserCreationForm):
-    email = CharField(required=True, max_length=254, label="E-Mail address")
-    nickname = CharField(required=True, max_length=12, label="Nickname")
-
-    class Meta:
-        model = CustomUser
-        fields = ("username", "nickname", "email", "password1", "password2")
-
-    def save(self, commit=True):
-        user = super(CustomForm, self).save(commit=False)
-        user.nickname = self.cleaned_data["nickname"]
-        if commit:
-            user.save()
-        return user
-
-    def clean_nickname(self):
-        if not self.cleaned_data["nickname"]:
-            raise ValidationError("Please Enter your nickname.")
-        if len(self.cleaned_data["nickname"]) > 12:
-            raise ValidationError("Nickname must be <= 12 char.")
-        return self.cleaned_data["nickname"]
-
-
 class SignUp(generic.CreateView):
-    form_class = CustomForm
+    form_class = CustomUserCreateForm
     success_url = reverse_lazy("login")
     template_name = "account/signup.html"
 
